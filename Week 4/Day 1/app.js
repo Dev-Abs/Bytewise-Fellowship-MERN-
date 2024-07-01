da344d7996e255969bd8580cf12b91e3ca96cbf1
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-01 13:36:38 +0500

    javascript concepts learned along comments about each consept

--- Week 4/Day 1/app.js
+++ Week 4/Day 1/app.js
@@ -139,6 +139,9 @@ greet();
 // Comma operator
 // Unary operators
 // Relational operators
+
+
+
 // Arithmetic operators
 // Arithmetic operators are used to perform arithmetic calculations on numbers.
 
@@ -271,16 +274,447 @@ console.log(!(x3 > 3 && x3 < 10)); // false
 // >>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
 // >>>	Zero fill right shift	Shift right by pushing zeros in from the left, and let the rightmost bits fall off
 
-let x4 = 5; // 101
-let y4 = 10; // 1010
+// let x4 = 5; // 101
+// let y4 = 10; // 1010
 
-console.log(x4 & y4); // 0
-console.log(x4 | y4); // 15
-console.log(~x4); // -6
-console.log(x4 ^ y4); // 15
-console.log(x4 << 1); // 10
-console.log(x4 >> 1); // 2
-console.log(x4 >>> 1); // 2
+// console.log(x4 & y4); // 0
+// console.log(x4 | y4); // 15
+// console.log(~x4); // -6
+// console.log(x4 ^ y4); // 15
+// console.log(x4 << 1); // 10
+// console.log(x4 >> 1); // 2
+// console.log(x4 >>> 1); // 2
+
+// String operators
+// The + operator can also be used to add (concatenate) strings.
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName + " " + lastName); // John Doe
+
+// Conditional (ternary) operator
+// The conditional operator assigns a value to a variable based on a condition.
+
+// Syntax
+// variable = (condition) ? value1 : value2
+
+let age1 = 20;
+let status = (age1 >= 18) ? "adult" : "minor"; // adult
+console.log(status);
+
+// Comma operator
+// The comma operator is used to evaluate multiple expressions. 
+// how it works ?
+// The comma operator evaluates each of its operands (from left to right) and returns the value of the last operand.
+
+
+let a2 = 1;
+let b2 = 2;
+let c2 = 3;
+
+let d = (a2, b2, c2);
+console.log(d); // 3
+
+// Unary operators
+// Unary operators are used to perform operations on a single operand.
+
+// The following unary operators are available in JavaScript:
+
+// Operator	Description
+// +	Converts an operand into a number
+// -	Negates an operand
+// ++	Increment
+// --	Decrement
+// !	Logical NOT
+
+let x5 = 5;
+let y5 = -x5;
+console.log(y5); // -5
+
+let z5 = "5";
+let a5 = +z5;
+console.log(a5); // 5
+
+let b5 = 5;
+b5++;
+console.log(b5); // 6
+
+let c5 = 5;
+c5--;
+console.log(c5); // 4
+
+let d5 = 5;
+let e5 = !d5;
+console.log(e5); // false
+
+// Relational operators
+// Relational operators are used to determine the relationship between two operands.
+
+// The following relational operators are available in JavaScript:
+
+// Operator	Description
+// in	Property in Object
+// instanceof	Instance of Object
+// <	Less than
+// >	Greater than
+// <=	Less than or equal to
+// >=	Greater than or equal to
+
+let cars = ["BMW", "Volvo", "Saab", "Ford"];
+console.log(0 in cars); // true, why ? because 0 is the index of the array
+
+let person1 = { firstName: "John", lastName: "Doe", age: 30 };
+console.log("firstName" in person1); // true, why ? because firstName is the key of the object
+
+let person2 = { firstName: "John", lastName: "Doe", age: 30 };
+console.log(person2 instanceof Object); // true , why ? because person2 is an object
+
+// Functions
+// A JavaScript function is a block of code designed to perform a particular task.
+
+// A JavaScript function is executed when "something" invokes it (calls it).
+
+// Example
+
+function greet() {
+    console.log("Hello");
+}
+greet(); // Hello
+
+// Function Parameters
+// Function parameters are the names listed in the function definition.
+
+// Function arguments are the real values passed to the function.
+
+// Parameters are specified after the function name, inside the parentheses.
+
+// You can add as many parameters as you want, just separate them with a comma.
+
+// The following example has a function with one parameter (fname). When the function is called, we pass along a first name, which is used inside the function to output the full name:
+
+function greet1(fname) {
+    console.log("Hello " + fname);
+}
+greet1("John"); // Hello John
+
+// The following example has a function with two parameters (fname, lname). When the function is called, we pass along two first names, which are used inside the function to output the full names:
+
+function greet2(fname, lname) {
+    console.log("Hello " + fname + " " + lname);
+}
+
+greet2("John", "Doe"); // Hello John Doe
+
+// Function Return
+
+// When JavaScript reaches a return statement, the function will stop executing.
+
+// If the function was invoked from a statement, JavaScript will "return" to execute the code after the invoking statement.
+
+// Functions often compute a return value. The return value is "returned" back to the "caller":
+
+function add(a, b) {
+    return a + b;
+}
+
+let sum1 = add(5, 10);
+console.log(sum1); // 15
+
+// Objects
+// JavaScript objects are containers for named values called properties or methods.
+
+// Properties are values associated with an object.
+
+// Methods are actions that can be performed on objects.
+
+// Example
+
+let person3 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+console.log(person3.firstName); // John
+console.log(person3.age); // 30
+console.log(person3.fullName()); // John Doe
+
+// Arrays
+// JavaScript arrays are used to store multiple values in a single variable.
+
+// Example
+
+let fruits1 = ["Apple", "Banana", "Mango"];
+console.log(fruits1[0]); // Apple
+console.log(fruits1[1]); // Banana
+console.log(fruits1[2]); // Mango
+
+// Loops
+// Loops can execute a block of code a number of times.
+
+// Loops are handy, if you want to run the same code over and over again, each time with a different value.
+
+// Different Kinds of Loops
+
+// JavaScript supports different kinds of loops:
+
+// for - loops through a block of code a number of times
+// for/in - loops through the properties of an object
+// for/of - loops through the values of an iterable object
+// while - loops through a block of code while a specified condition is true
+// do/while - also loops through a block of code while a specified condition is true
+
+// for loop
+// The for loop has the following syntax:
+
+// for (statement 1; statement 2; statement 3) {
+//   // code block to be executed
+// }
+
+// Statement 1 is executed (one time) before the execution of the code block.
+
+// Statement 2 defines the condition for executing the code block.
+
+// Statement 3 is executed (every time) after the code block has been executed.
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// for/in loop
+// The for/in loop has the following syntax:
+
+// for (variable in object) {
+//   // code block to be executed
+// }
+
+let person4 = { name: "John", age: 30, city: "New York" };
+
+for (let x in person4) {
+    console.log(person4[x]); // John 30 New York
+}
+
+// for/of loop
+// The for/of loop has the following syntax:
+
+// for (variable of iterable) {
+//   // code block to be executed
+// }
+
+let fruits2 = ["Apple", "Banana", "Mango"];
+
+for (let x of fruits2) {
+    console.log(x); // Apple Banana Mango
+}
+
+// while loop
+
+// The while loop loops through a block of code as long as a specified condition is true.
+
+// The following syntax is used for the while loop:
+
+// while (condition) {
+//   // code block to be executed
+// }
+
+let i = 0;
+
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// do/while loop
+
+// The do/while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+
+// The following syntax is used for the do/while loop:
+
+// do {
+//   // code block to be executed
+// }
+// while (condition);
+
+let j = 0;
+
+do {
+    console.log(j);
+    j++;
+}
+while (j < 5);
+
+// Conditional Statements
+
+// Conditional statements are used to perform different actions based on different conditions.
+
+// In JavaScript we have the following conditional statements:
+
+// Use if to specify a block of code to be executed, if a specified condition is true
+// Use else to specify a block of code to be executed, if the same condition is false
+// Use else if to specify a new condition to test, if the first condition is false
+// Use switch to specify many alternative blocks of code to be executed
+
+// if statement
+// The if statement has the following syntax:
+
+// if (condition) {
+//   // code block to be executed
+// }
+
+let time = 20;
+
+if (time < 18) {
+    console.log("Good day"); //
+}
+
+// else statement
+// The else statement has the following syntax:
+
+// if (condition) {
+//   // code block to be executed
+// } else {
+//   // code block to be executed
+// }
+
+let time1 = 20;
+
+if (time1 < 18) {
+    console.log("Good day");
+}
+else {
+    console.log("Good evening"); // Good evening
+}
+
+// else if statement
+
+// The else if statement has the following syntax:
+
+// if (condition1) {
+//   // code block to be executed
+// } else if (condition2) {
+//   // code block to be executed
+// } else {
+//   // code block to be executed
+// }
+
+let time2 = 20;
+
+if (time2 < 18) {
+    console.log("Good day");
+}
+else if (time2 < 20) {
+    console.log("Good evening");
+}
+else {
+    console.log("Good night");
+}
+
+
+// switch statement
+// The switch statement is used to perform different actions based on different conditions.
+
+// The following syntax is used for the switch statement:
+
+// switch (expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
+let day = 2;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Another day");
+}
+
+// Type Conversion
+// JavaScript is a loosely typed or a dynamic language. This means that you don't have to declare the type of a variable ahead of time.
+// implicit convesion
+let x6 = 5;
+let y6 = "5";
+
+console.log(x6 + y6); // 55
+
+console.log(6 - '5'); // 1
+
+// explicit conversion
+let x7 = 5;
+let y7 = "5";
+
+console.log(x7 + parseInt(y7)); // 10
+console.log(6 - parseInt('5')); // 1
+
+// JavaScript Type Operators
+// There are two JavaScript type operators:
+
+// typeof
+// instanceof
+
+// typeof operator
+// The typeof operator returns the type of a variable or an expression.
+
+let x8 = 5;
+let y8 = "Hello";
+
+console.log(typeof x8); // number
+console.log(typeof y8); // string
+
+// instanceof operator
+
+// The instanceof operator returns true if an object is an instance of an object type.
+
+let cars1 = ["BMW", "Volvo", "Saab", "Ford"];
+console.log(cars1 instanceof Array); // true
+
+
+// scope
+// In JavaScript, scope refers to the current context of your code. Scopes can be globally or locally defined.
+
+// Global Scope
+// Variables declared outside a function become global variables (all scripts and functions on a web page can access it).
+
+// Local Scope
+// Variables declared within a JavaScript function become local to the function.
+
+// Scope determines the accessibility (visibility) of variables.
+
+// Variables defined inside a function are not accessible (visible) from outside the function.
+
+// Example
+
+// code here can NOT use carName
+
+function myFunction() {
+    let carName = "Volvo";
+
+    // code here CAN use carName
+}
+
+// code here can NOT use carName
+
+
+
+
+
 
 
 
