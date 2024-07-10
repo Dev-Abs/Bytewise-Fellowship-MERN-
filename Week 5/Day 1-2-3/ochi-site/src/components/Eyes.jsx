94399638b7dd1d3d3d7e5b6ac8946b9a89e1210c
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-10 18:03:38 +0500

    eyes component created

---
+++ Week 5/Day 1-2-3/ochi-site/src/components/Eyes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Eyes = () => {
+  return (
+    <div className='w-full h-screen eyes overflow-hidden '>
+        <div className='w-full h-full bg-cover bg-center bg-no-repeat bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] '></div>
+    </div>
+  )
+}
+
+export default Eyes
