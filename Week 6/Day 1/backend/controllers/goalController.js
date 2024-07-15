39cf221483c0c0d583a71406fbff0170ce68170b
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-15 15:48:35 +0500

    goals and routes setup completed

--- Week 6/Day 1/backend/controllers/goalController.js
+++ Week 6/Day 1/backend/controllers/goalController.js
@@ -1,39 +1,51 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc: Get Goals
 // @route: GET /api/goals
 // @access: Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler( async (req, res) => { //using async, before getting into the database, becuase it will take some time to get the data
     res.status(200).json({
         message: 'Get Goals'
     })
-}
+})
 
 // @desc: Post Goals
 // @route: POST /api/goals
 // @access: Private
-const postGoals = (req, res) => {
+const postGoals = asyncHandler (async (req, res) => {
+    if (!req.body.title) {
+        res.status(400)
+        throw new Error('Please fill the title')
+    }
+
     res.status(200).json({
         message: 'Post Goals'
     })
-}
+})
 
 // @desc: Put Goals by ID
 // @route: PUT /api/goals/:id
 // @access: Private
-const putGoals = (req, res) => {
+const putGoals = asyncHandler (async (req, res) => {
     res.status(200).json({
         message: `Put Goals by ID ${req.params.id}`
     })
-}
+})
 
 // @desc: Delete Goals by ID
 // @route: DELETE /api/goals/:id
 // @access: Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler (async (req, res) => {
     res.status(200).json({
         message: `Delete Goals by ID ${req.params.id}`
     })
-}
+})
 
 module.exports = {
     getGoals, postGoals, putGoals, deleteGoals  
 }
+
+
+
+// install express-async-handler
+// because we are using async await in our controller
--- Week 6/Day 1/backend/server.js
+++ Week 6/Day 1/backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-
+const {errorHandler} = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// in order to use req.body we need to add this line
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/goals', require('./routes/goalRoutes'));
 
-
-
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
