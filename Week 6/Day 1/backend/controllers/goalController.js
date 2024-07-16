94866a2b3f44e96987223e481a3a0c73419ce1b8
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-16 18:15:42 +0500

    goal routes authenticated and controller tailored for authorized users

--- Week 6/Day 1/backend/controllers/goalController.js
+++ Week 6/Day 1/backend/controllers/goalController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 // @desc: Get Goals
 // @route: GET /api/goals
 // @access: Private
 const getGoals = asyncHandler( async (req, res) => { //using async, before getting into the database, becuase it will take some time to get the data
-    const goals = await Goal.find({}) //find all the goals    
+    const goals = await Goal.find({ user: req.user.id }) //find all the goals    
         res.status(200).json(goals) //send the goals in json format
 })
 
@@ -16,7 +17,10 @@ const postGoals = asyncHandler (async (req, res) => {
         res.status(400)
         throw new Error('Please add text field')
     }
-    const goal = new Goal({text: req.body.text})
+    const goal = new Goal({
+        text: req.body.text,
+        user: req.user.id
+    })
     const createdGoal = await goal.save()
     res.status(200).json(createdGoal)
 })
@@ -29,6 +33,20 @@ const putGoals = asyncHandler (async (req, res) => {
     if (!goal) {
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // check for user
+    if (!user) {
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+    // Make sure user is goal owner, logged in user matches the user who created the goal (goal user)
+    if (goal.user.toString() !== req.user.id) { // goal.user is an object, so we need to convert it to string, why we are using goal.user and how? , because we have user field in goal model
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -48,8 +66,24 @@ const deleteGoals = asyncHandler (async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
-    // await goal.destroy() // not working, becuse we are using mongoose version above 5
 
+    
+    // Get user
+    const user = await User.findById(req.user.id)
+
+    // check for user
+    if (!user) {
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+    // Make sure user is goal owner, logged in user matches the user who created the goal (goal user)
+    if (goal.user.toString() !== req.user.id) { // goal.user is an object, so we need to convert it to string, why we are using goal.user and how? , because we have user field in goal model
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+    // await goal.destroy() // not working, becuse we are using mongoose version above 5
     await goal.deleteOne();
     
     res.status(200).json({message: 'Goal removed', id: req.params.id})
--- Week 6/Day 1/backend/routes/goalRoutes.js
+++ Week 6/Day 1/backend/routes/goalRoutes.js
@@ -3,17 +3,19 @@ const router = express.Router();
 
 const {getGoals, postGoals, putGoals, deleteGoals } = require('../controllers/goalController');
 
+const { protect } = require('../middleware/authMiddleware');
+
 // router.get('/', getGoals);
 
 // router.post('/', postGoals);
 
-router.route('/').get(getGoals).post(postGoals);
+router.route('/').get(protect, getGoals).post(protect, postGoals);
 
 // router.put('/:id', putGoals);
 
 // router.delete('/:id', deleteGoals);
 
-router.route('/:id').put(putGoals).delete(deleteGoals);
+router.route('/:id').put(protect, putGoals).delete(protect, deleteGoals);
 
 
 module.exports = router;
