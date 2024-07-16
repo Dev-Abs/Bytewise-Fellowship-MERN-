eef4cf11a447b40a4479f09ddf7162848e2c13fe
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-16 17:27:16 +0500

    token generation using jwt

--- Week 6/Day 1/backend/controllers/userController.js
+++ Week 6/Day 1/backend/controllers/userController.js
@@ -37,7 +37,7 @@ const registerUser = asyncHandler( async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -71,7 +71,7 @@ const loginUser = asyncHandler( async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
-        token: null
+        token: generateToken(user._id)
     })
 
     // if (user && (await bcrypt.compare(password, user.password))) {
@@ -100,5 +100,11 @@ const getUserProfile = asyncHandler( async (req, res) => {
 }
 )
 
+// Generate JWT 
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
 
 module.exports = {registerUser, loginUser, getUserProfile}
