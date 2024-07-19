47b6677999088b329d02590b1e5593d9b24f3f26
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-19 12:55:43 +0500

    Login component created

--- Week 6/Day 1/goalapp-frontend/src/App.js
+++ Week 6/Day 1/goalapp-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <>
     <Navbar />
+    <Login  />
     </>
   )
 }
