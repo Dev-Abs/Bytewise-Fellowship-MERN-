8cf38309d8da2a28fa6895e173c18ec3261ec033
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-19 12:46:15 +0500

    react app created and tailwind setup completed

---
+++ Week 6/Day 1/goalapp-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline bg-gray-800 h-screen">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
