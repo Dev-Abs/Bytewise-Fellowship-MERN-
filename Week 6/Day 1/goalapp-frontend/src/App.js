14591dd47de5c5b4ccf87cad20551d711fcec543
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-19 13:00:40 +0500

    Signup component completed

--- Week 6/Day 1/goalapp-frontend/src/App.js
+++ Week 6/Day 1/goalapp-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
     <>
     <Navbar />
-    <Login  />
+    {/* <Login  /> */}
+    <Signup />
     </>
   )
 }
--- Week 6/Day 1/goalapp-frontend/src/components/Signup.jsx
+++ Week 6/Day 1/goalapp-frontend/src/components/Signup.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className="flex justify-center items-center h-screen bg-gray-200">
+            <div className="bg-white p-16 rounded shadow-2xl w-1/3">
+                <h2 className="text-3xl font-bold mb-10 text-gray-800">Signup</h2>
+                <form action="">
+                    {/* name */}
+                    <div className="mb-6">
+                        <label htmlFor="name" className="block text-gray-800 font-bold">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Your Name" className="w-full border-2 border-gray-300 p-3 rounded outline-none focus:border-purple-500" />
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="email" className="block text-gray-800 font-bold">Email</label>
+                        <input type="text" id="email" name="email" placeholder="Your Email" className="w-full border-2 border-gray-300 p-3 rounded outline-none focus:border-purple-500" />
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="password" className="block text-gray-800 font-bold">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Your Password" className="w-full border-2 border-gray-300 p-3 rounded outline-none focus:border-purple-500" />
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="password" className="block text-gray-800 font-bold">Confirm Password</label>
+                        <input type="password" id="password" name="password" placeholder="Confirm Password" className="w-full border-2 border-gray-300 p-3 rounded outline-none focus:border-purple-500" />
+                    </div>
+                    <button type="submit" className="w-full py-3 bg-purple-600 text-white rounded hover:bg-purple-500">Signup</button>
+                </form>
+            </div>
+        </div>
+    </>
   )
 }
 
