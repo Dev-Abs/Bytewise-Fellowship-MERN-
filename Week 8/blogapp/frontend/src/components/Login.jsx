c3d67bf85f63a0744b616502b2dac569ad08bb2a
Dev-Abs <abdullah.devorbit@gmail.com>
2024-08-01 00:55:44 +0500

    login signup design

--- Week 8/blogapp/frontend/src/components/Login.jsx
+++ Week 8/blogapp/frontend/src/components/Login.jsx
@@ -1,48 +1,151 @@
 // Login.js
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { loginUser } from '../features/users/loginSlice'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../features/users/loginSlice";
+import logo from "../assets/blog.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const dispatch = useDispatch()
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    })
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const { loading, error, user } = useSelector((state) => state.login)
+  const { loading, error, user } = useSelector((state) => state.login);
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        })
-    }
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        dispatch(loginUser(formData))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(formData));
+  };
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-            {loading && <div>Loading...</div>}
-            {error && <div>Error: {error}</div>}
-            {user && <div>Logged In</div>}
+  return (
+    // <div>
+    //     <h1>Login</h1>
+    //     <form onSubmit={handleSubmit}>
+    //         <div>
+    //             <label>Email:</label>
+    //             <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+    //         </div>
+    //         <div>
+    //             <label>Password:</label>
+    //             <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+    //         </div>
+    //         <button type="submit">Login</button>
+    //     </form>
+    //     {loading && <div>Loading...</div>}
+    //     {error && <div>Error: {error}</div>}
+    //     {user && <div>Logged In</div>}
+    // </div>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img alt="Your Company" src={logo} className="mx-auto h-10 w-auto" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
         </div>
-    )
-}
 
-export default Login
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  autoComplete="email"
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+          {loading && (
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Loading
+            </a>
+          )}
+
+          {user && (
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Logged In
+            </a>
+          )}
+
+          {error && (
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Error: {error}
+            </a>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- Week 8/blogapp/frontend/src/components/SignUp.jsx
+++ Week 8/blogapp/frontend/src/components/SignUp.jsx
@@ -1,54 +1,165 @@
 // Signup.js
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { signupUser } from '../features/users/signupSlice'
-
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signupUser } from "../features/users/signupSlice";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/blog.png";
 const Signup = () => {
-    const dispatch = useDispatch()
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-    })
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const { loading, error, value } = useSelector((state) => state.signup)
+  const { loading, error, value } = useSelector((state) => state.signup);
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        })
-    }
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log('formdata:' ,formData)
-        dispatch(signupUser(formData))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("formdata:", formData);
+    dispatch(signupUser(formData));
+  };
 
-    return (
-        <div>
-            <h1>Signup</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name:</label>
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-                </div>
-                <button type="submit">Signup</button>
-            </form>
-            {loading && <div>Loading...</div>}
-            {error && <div>Error: {error}</div>}
-            {value && <div>Signup successful!</div>}
+  return (
+    // <div>
+    //     <h1>Signup</h1>
+    //     <form onSubmit={handleSubmit}>
+    //         <div>
+    //             <label>Name:</label>
+    //             <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+    //         </div>
+    //         <div>
+    //             <label>Email:</label>
+    //             <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+    //         </div>
+    //         <div>
+    //             <label>Password:</label>
+    //             <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+    //         </div>
+    //         <button type="submit">Signup</button>
+    //     </form>
+    //     {loading && <div>Loading...</div>}
+    //     {error && <div>Error: {error}</div>}
+    //     {value && <div>Signup successful!</div>}
+    // </div>
+
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img alt="Your Company" src={logo} className="mx-auto h-10 w-auto" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign Up
+          </h2>
         </div>
-    )
-}
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                  autoComplete="name"
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  autoComplete="email"
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                />
+              </div>
+            </div>
 
-export default Signup
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+          {loading && (
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Loading...
+            </a>
+          )}
+          {error && (
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Error: {error}
+            </a>
+          )}
+          {value && (
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              SignUp Successful
+            </a>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
