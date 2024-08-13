fd489415e1dcf75e2d55d69542dd90a7cb682715
Dev-Abs <abdullah.devorbit@gmail.com>
2024-08-13 14:02:18 +0500

    mongo connection

--- Week 10/Day 1-2/server/index.js
+++ Week 10/Day 1-2/server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
 
 app.use(
   "/graphql",
